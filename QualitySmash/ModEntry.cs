0516aec5edc300f60734f51c640a4de59bf95bd4
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 20:02:10 -0500

    Add project files.

---
+++ QualitySmash/ModEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace QualitySmash
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+    }
+}
